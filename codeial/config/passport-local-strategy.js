faabc20c2ff5d793370605bee4c422a0665871ad
ashutosh kumar <kumarashutosh7302401472@gmail.com>
2023-09-12 15:53:22 +0530

    here i changed my code from simple to async

--- codeial/config/passport-local-strategy.js
+++ codeial/config/passport-local-strategy.js
@@ -1,31 +1,35 @@
-const passport = require('passport');
 
-const LocalStrategy = require('passport-local').Strategy;
-
-const User = require('../models/user')
 
 // authentication using passport
+// Import necessary modules and User model
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user'); // Adjust the path as needed
+
+// Configure Passport to use the LocalStrategy
 passport.use(new LocalStrategy({
-    usernameField: 'email'
+  usernameField: 'email'
 },
-function(email, password, done){
-// find the user and establish the  identity
-User.findOne({email: email}, function(err, user){
-    if(err){
-        console.log("error in finding the user by passport in the code");
-        return done(err);
+async (email, password, done) => {
+  try {
+    // Find the user by email using async/await
+    const user = await User.findOne({ email });
+
+    if (!user || user.password !== password) {
+      // User not found or password doesn't match
+      console.log('Invalid Username/Password');
+      return done(null, false);
     }
-    if(!user || user.password != password){
-        console.log('invalid Username/password');
-        return done(null, false);
-    }
 
-    return done(null , user);
-});
+    // User found and password matches, return the user
+    return done(null, user);
+  } catch (err) {
+    // Handle any errors that occur during the query
+    console.log('Error in finding the user by Passport in the code');
+    return done(err);
+  }
+}));
 
-}
-
-));
 
 //serializing the user to decide which keyu is to kept in the cookies 
 passport.serializeUser(function(user, done ){
@@ -33,14 +37,33 @@ passport.serializeUser(function(user, done ){
 });
 
 
-//deserialzing the user form the key in the cookies  
-passport.deserializeUser(function(id , done){
-    User.findById(id, function(err, user){
-if(err){
-    console.log('error in finding the user using passport')
-}
-return done(null, user)
-    });
+//deserialzing the user form the key in the cookies  adn there was error while using this code in the termuinal because of tyhe version problem in the code so because of this we are using async and wait function in the code 
+// passport.deserializeUser(function(id , done){
+//     User.findById(id, function(err, user){
+// if(err){
+//     console.log('error in finding the user using passport')
+// }
+// return done(null, user)
+//     });
+// });
+// deserialzing the user from the key in the cookies
+
+
+
+
+ 
+passport.deserializeUser(async function (id, done) {
+    try {
+        const user = await User.findById(id);
+        if (!user) {
+            console.log('User not found');
+            return done(null, false);
+        }
+        return done(null, user);
+    } catch (err) {
+        console.log('Error in finding the user using Passport');
+        return done(err);
+    }
 });
 
 
--- codeial/index.js
+++ codeial/index.js
@@ -4,23 +4,34 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
-//used for session cookie 
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 
+// Middleware Order: Use session middleware before initializing Passport
+app.use(session({
+    name: 'codeial',
+    secret: 'besomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ... Other middleware and configurations
 
 app.use(express.urlencoded());
-
 app.use(cookieParser());
-
 app.use(express.static('./assets'));
-
 app.use(expressLayouts);
+
 // extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
-
 
 // use express router
 app.use('/', require('./routes'));
@@ -29,9 +40,8 @@ app.use('/', require('./routes'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
-app.listen(port, function(err){
-    if (err){
+app.listen(port, function (err) {
+    if (err) {
         console.log(`Error in running the server: ${err}`);
     }
 
