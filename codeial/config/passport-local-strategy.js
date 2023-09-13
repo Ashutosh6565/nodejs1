73c14dabbb880b70983a0bc3c6fadefc0c423384
ashutosh kumar <kumarashutosh7302401472@gmail.com>
2023-09-13 15:52:00 +0530

    install mongo to connect mongoose to store cookies

--- codeial/config/passport-local-strategy.js
+++ codeial/config/passport-local-strategy.js
@@ -77,6 +77,13 @@ passport.checkAuthentication = function(req, res ,next){
   return res.redirect('/users/sign-in')
 
 }
+passport.setAuthenticatedUser = function(req, res, next){
+  if(req.isAuthenticated()){
+    res.locals.user = req.user;
+  }
+
+  next()
+}
 
 
 module.exports = passport;
