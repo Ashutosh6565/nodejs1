0eb9c152b9b942f437f9c4b71445f590afb3c861
ashutosh kumar <kumarashutosh7302401472@gmail.com>
2023-09-19 18:22:17 +0530

    installed env file and dotenv

--- codeial/index.js
+++ codeial/index.js
@@ -7,8 +7,12 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-
+const MongoStore = require('connect-mongo');
+require('dotenv').config()
 // Middleware Order: Use session middleware before initializing Passport
+
+//mongo store is used to store the session cookie in the db 
+
 app.use(session({
     name: 'codeial',
     secret: 'besomething',
@@ -16,7 +20,18 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore(
+        {
+        // mongooseConnection: db,
+            mongoUrl: process.env.MONGO_URI,
+            autoRemove: 'disabled'
+        
+    },
+    function(err){
+        console.log(err | 'connect-mongodb setup ok')
     }
+    )
 }));
 
 app.use(passport.initialize());
